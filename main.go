9f8282cb29a8dc6d16bea4aedbc13eba0eeff509
Jesse <jessebot.git@gmail.com>
2023-06-06 19:43:50 -0700

    remove rename command

--- main.go
+++ main.go
@@ -17,60 +17,51 @@ func main() {
 		log.Fatal("Missing arguments. See command `help` for more information.")
 	}
 
-	command := os.Args[1]
-	options := os.Args[2:]
-
-	if command == "help" || command == "-h" || command == "--help" {
+	if os.Args[1] == "help" {
 		usage()
-	} else if command == "version" || command == "--version" || command == "-V" {
-		fmt.Printf("rbook v%v\n", Version)
-	} else if command == "r" || command == "rename" {
-		if len(options) < 2 {
-			log.Fatal("Missing arguments. Must provide input_file, title and author.")
-		}
+		return
+	}
 
-		inputFile := options[0]
-		title := options[1]
-		author := options[2]
-		tags := options[3:]
-		extension := filepath.Ext(inputFile)
+	options := os.Args[1:]
 
-		fname := createFileName(title, author, tags) + extension
-		err := os.Rename(inputFile, fname)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		log.Fatal("Invalid command", command, "See command `help` for more information.")
+	if len(options) < 2 {
+		log.Fatal("Missing arguments. Must provide input_file, title and author.")
+	}
+
+	inputFile := options[0]
+	title := options[1]
+	author := options[2]
+	tags := options[3:]
+	extension := filepath.Ext(inputFile)
+
+	fname := createFileName(title, author, tags) + extension
+
+	err := os.Rename(inputFile, fname)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
 func createFileName(title string, author string, tags []string) string {
 	sort.Strings(tags)
 	excludedPunctuationRegexp := regexp.MustCompile("[][{}!@#$%^&*()=+'\"?,.|;:~`‘’“”/]*")
-	var result string
 
-	lowercasedTitle := strings.Trim(strings.ToLower(title), " ")
-	lowercasedAuthor := strings.Trim(strings.ToLower(author), " ")
+	sanitizedTitle := strings.Trim(strings.ToLower(title), " ")
+	sanitizedAuthor := strings.Trim(strings.ToLower(author), " ")
 
-	result = excludedPunctuationRegexp.ReplaceAllString(lowercasedTitle, "")
-	result += "__" + excludedPunctuationRegexp.ReplaceAllString(lowercasedAuthor, "")
+	result := excludedPunctuationRegexp.ReplaceAllString(sanitizedTitle, "") +
+		"__" + excludedPunctuationRegexp.ReplaceAllString(sanitizedAuthor, "")
 
 	for _, tag := range tags {
-		lowercasedTag := strings.Trim(strings.ToLower(tag), " ")
-		sanitizedTag := excludedPunctuationRegexp.ReplaceAllString(lowercasedTag, "")
-		result += "_" + sanitizedTag
+		trimmedTag := strings.Trim(tag, " ")
+		sanitizedTag := excludedPunctuationRegexp.ReplaceAllString(trimmedTag, "")
+		result += "_" + strings.ToLower(sanitizedTag)
 	}
 
 	return strings.ReplaceAll(result, " ", "-")
 }
 
 func usage() {
-	fmt.Println("USAGE: rbook <command>.")
+	fmt.Println("USAGE: rb <input_file|help|>")
 	fmt.Println("")
-	fmt.Println("COMMANDS")
-	fmt.Println("--------")
-	fmt.Println("[r]ename <input_file> [options] [tags]: rename input_file; no options = interactive mode")
-	fmt.Println("help: print usage information (this)")
-	fmt.Println("version: print version")
 }
